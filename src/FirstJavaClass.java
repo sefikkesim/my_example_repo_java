c54552ff8e9c9db98d4824b22585848782bb683d
sefikkesim <sefikkesim@gmail.com>
2022-03-26 21:54:06 +0200

    Third Commit

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -1,5 +1,6 @@
 public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("First Java Github attempt");
+        System.out.println("Please tyr again");
     }
 }
